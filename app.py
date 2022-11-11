268253c609ace51fc40432be77c225e0af239f64
IanDam <111082036+IanDam@users.noreply.github.com>
2022-11-11 13:44:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ import numpy as np
 st.title("Proyecto 12")
 st.write(" En la presente pagina, se presenta una presentacion de un indice de felicidad constriudo a partir de 10 diferentes variables de 60 paises ")
 st.subheader("Explicacion variables usadas: ")
-st.write("""1:
-2:
-3:
-4:
-5:
-6:
-7:
-8:
-9:
-10:""")
+st.write("1:Inflacion")
+st.write("2:IDH")
+st.write("3:PIB per Capita")
+st.write("4:")
+st.write("5:Tasa de suicidios")
+st.write("6:Salario medio de los empleados")
+st.write("7:Desempleo total")
+st.write("8:Esperanza de vida")
+st.write("9:Poblacion mayor a los 65 años")
+st.write("10:Tasa de homicidios intencionales")
+
+st.subheader("Reduccion de dimencionalidad (PCA): ")
+st.subheader("Cluster y Kmeans:")
