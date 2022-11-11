de4df74aaa4e33f4bf6be7920bc88402313445ea
IanDam <111082036+IanDam@users.noreply.github.com>
2022-11-11 13:30:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import wbgapi as wb
+from sklearn.linear_model import LinearRegression
+st.title("Proyecto 6")
+st.write(" En la presente pagina, a partir de un dataset dentro del que se determina la potabilidad del agua a partir de caracteristicas especificas como la cantidad de carbono organico o su condutividad, se realizan lo siguiente: ")
+st.subheader("Exploracion inicial: ")
+st.write("Para la exploración inicial obtenemos información acerca del dataset cargado. En este encontramos 3276 entradas y un total de 10 variables dentro de ella (columnas). Así mismo vemos que de las 10 columnas de datos; 1 es de typo int, 9 de tipo float.Adicionalmente se observa que existen datos nulos en las variables ph, Sulfate y Trihalomethanes.")
