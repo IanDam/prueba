2a762d108a336765e58d6f1463334a59698b26c7
IanDam <111082036+IanDam@users.noreply.github.com>
2022-11-11 22:28:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from pandas import DataFrame
+from numpy.random import random
+import plotly.graph_objects as go
 
+data1 = DataFrame(random((100, 3)))
+data2 = DataFrame(random((100, 3)))
+
+trace1 = [go.Scatter3d(x=data1[0], y=data1[1], z=data1[2])]
+trace2 = [go.Scatter3d(x=data2[0], y=data2[1], z=data2[2])]
+
+fig = go.Figure(trace1 + trace2, layout=go.Layout(uirevision='foo'))
+
+title = st.text_input('Movie title', 'Life of Brian')
+st.plotly_chart(fig, use_container_width=True)
 st.title("Proyecto 12")
 st.write(" En la presente pagina, se presenta una presentacion de un indice de felicidad constriudo a partir de 10 diferentes variables de 60 paises ")
 st.subheader("Explicacion variables usadas: ")
